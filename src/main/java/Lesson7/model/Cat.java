48e329a7891a61230930ce011e0c0fe7ca84aec3
CAHbKA <votgde@mail.ru>
2021-02-06 01:01:30 +0800

    HW7 1. Содать классы кот и тарелка, создать их объекты или массивы объектов в зависимости от задачи 2. Сделать так, чтобы в тарелке с едой не могло получиться отрицательного количества еды (например, в миске 10 еды, а кот пытается покушать 15-20). 3. Каждому коту нужно добавить поле сытость (когда создаем котов, они голодны). Если коту удалось покушать (хватило еды), сытость = true. 4. Считаем, что если коту мало еды в тарелке, то он её просто не трогает, то есть не может быть наполовину сыт (это сделано для упрощения логики программы). 5. Создать массив котов и тарелку с едой, попросить всех котов покушать из этой тарелки и потом вывести информацию о сытости котов в консоль. 6. Добавить в тарелку метод, с помощью которого можно было бы добавлять еду в тарелку.1. Содать классы кот и тарелка, создать их объекты или массивы объектов в зависимости от задачи                                                                                       2. Сделать так, чтобы в тарелке с едой не могло получиться отрицательного количества еды (например, в миске 10 еды, а кот пытается покушать 15-20).                                                                                       3. Каждому коту нужно добавить поле сытость (когда создаем котов, они голодны). Если коту удалось покушать (хватило еды), сытость = true.                                                                                       4. Считаем, что если коту мало еды в тарелке, то он её просто не трогает, то есть не может быть наполовину сыт (это сделано для упрощения логики программы).                                                                                       5. Создать массив котов и тарелку с едой, попросить всех котов покушать из этой тарелки и потом вывести информацию о сытости котов в консоль.                                                                                       6. Добавить в тарелку метод, с помощью которого можно было бы добавлять еду в тарелку.

---
+++ src/main/java/Lesson7/model/Cat.java
@@ -0,0 +1,41 @@
+package Lesson7.model;
+
+public class Cat {
+
+    private boolean isSatiety ; // сытость
+    private int stomachSize; // размер желудка
+
+    public Cat() {// дефолтный кот
+        this.isSatiety = false;
+        this.stomachSize = 10;
+    }
+
+    public Cat(int stomachSize) {
+        this.isSatiety = false;
+        this.stomachSize = stomachSize;
+    }
+
+    public boolean isSatiety() {
+        return isSatiety;
+    }
+
+    public int getStomachSize() {
+        return stomachSize;
+    }
+
+    public static void eat(Plate plate, Cat cat) {
+
+        if (cat.isSatiety()) {
+            System.out.println("Кот уже сытый по горло. Пошел мимо миски");
+        } else
+            // хватит ли еды коту
+            if (plate.getFilling() >= cat.getStomachSize()) {
+                plate.setFilling(plate.getFilling() - cat.getStomachSize());
+                System.out.println("поел. В миске осталось " + plate.getFilling() + " корма");
+                cat.isSatiety = true;
+            } else System.out.println("Еды мало. Сыпани еще.");
+
+        System.out.println();
+
+    }
+}
---
+++ src/main/java/Lesson7/model/Plate.java
@@ -0,0 +1,64 @@
+package Lesson7.model;
+
+import java.util.Random;
+
+public class Plate {
+
+
+    private int filling;
+    private int volume;
+    private boolean isFull;
+
+
+    public Plate() {
+        this.filling = 0;
+        this.volume = 70;
+        this.isFull = false;
+    }
+
+    public Plate(int filling, int volume) {
+        this.filling = filling;
+        this.volume = volume;
+        this.isFull = filling >= volume;
+
+    }
+
+    public boolean isFull() {
+        return isFull;
+    }
+
+    public int getFilling() {
+        return filling;
+    }
+
+    public void setFilling(int filling) {
+
+        this.filling = filling;
+        if (filling == 0) this.isFull = false;
+        if (filling > this.volume) this.isFull = true;
+
+    }
+
+    public void fillingPlate() {
+        Random random = new Random();
+        this.filling = this.filling + random.nextInt(40);
+        if (filling < volume) {
+            System.out.println("Чашка не полная, сыпани еще! " + filling + " из " + volume);
+            //isEmpty = false;
+            isFull = false;
+        }
+        if (filling == volume) {
+            System.out.println("Тарелка полная");
+            isFull = true;
+        }
+        if (filling > volume) {
+            System.out.println("Корм посыпался через край! " + volume + " в миске. Еще " + (filling - volume) + " на полу (коты с пола не едят. Выкидываем)");
+            filling = volume;
+            isFull = true;
+        }
+
+        System.out.println();
+
+
+    }
+}
